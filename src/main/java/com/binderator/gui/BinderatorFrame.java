177f3ec4db942a5f6407cb5c21f6e7b95ba7c258
Paul Swoboda <paulswoboda@gmail.com>
2023-06-13 17:28:29 +1000

    Bumping the release version to 0.3.0.

--- src/main/java/com/binderator/gui/BinderatorFrame.java
+++ src/main/java/com/binderator/gui/BinderatorFrame.java
@@ -79,7 +79,7 @@ public class BinderatorFrame extends JFrame
 
   @Serial
   private static final long serialVersionUID = -1429747105438739695L;
-  public static final String VERSION = "0.2.3";
+  public static final String VERSION = "0.3.0";
   private static BinderatorFrame singletonInstance = null;
   private boolean haveUnsavedChanges = false;
   private static final FileFilter binderatorFileFilter =
